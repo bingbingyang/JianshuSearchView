5fbbf6e295701efaf490eecbb372683195d5dc99
bingyang <bingyang@163.com>
2018-03-11 16:26:13 +0800

    first commit

---
+++ app/src/main/java/com/yangbing/jianshudemo/Utils.java
@@ -0,0 +1,9 @@
+package com.yangbing.jianshudemo;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+public class Utils {
+    public static float dpToPixel(float dp) {
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        return dp * metrics.density;
+    }
+}
